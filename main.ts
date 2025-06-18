6e5190f34a0f07ea8fc5a92f9d04b2196aab1b42
mouna09876 <mounahajjaj35@gmail.com>
2025-06-18 11:13:59 +0200

    Aggiorna main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,3 +3,21 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 scene.setBackgroundColor(5)
 scene.setBackgroundColor(0)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 3 3 3 3 . . . . . 
+    . . . . 3 3 3 3 3 3 . 3 . . . . 
+    . . . 3 . 3 . 3 3 3 . 3 . . . . 
+    . . 3 . 3 . . . 3 3 3 . . . . . 
+    . 3 3 3 3 3 3 3 . . 3 . . . . . 
+    . . . 3 . . . . . . . 3 3 . . . 
+    . . . . 3 . . . . . . . . 3 . . 
+    . . . . . 3 3 . . . . . . 3 . . 
+    . . . . . . . 3 3 . . . . 3 . . 
+    . . . . . . . . . 3 3 . . 3 . . 
+    . . . . . . . . . . . 3 3 3 . . 
+    . . . . . . . . . . . . . 3 3 . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
