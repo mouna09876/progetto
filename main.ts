b0f0f0c182a23862aa4d84aa50209e76d7d1dd50
mouna09876 <mounahajjaj35@gmail.com>
2025-06-18 11:47:33 +0200

    Aggiorna main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,20 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
-})
 scene.setBackgroundColor(5)
-scene.setBackgroundColor(0)
-let mySprite = sprites.create(img`
-    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    2 2 2 2 2 2 2 3 3 3 3 2 2 2 2 2 
-    2 2 2 2 3 3 3 3 3 3 . 3 2 2 2 2 
-    2 2 2 3 . 3 . 3 3 3 . 3 2 2 2 2 
-    2 2 3 . 3 . . . 3 3 3 2 2 2 2 2 
-    2 3 3 3 3 3 3 3 . . 3 2 2 2 2 2 
-    2 2 2 3 . . . . . . . 3 3 2 2 2 
-    2 2 2 2 3 . . . . . . . . 3 2 2 
-    2 2 2 2 2 3 3 . . . . . . 3 2 2 
-    2 2 2 2 2 2 2 3 3 . . . . 3 2 2 
-    2 2 2 2 2 2 2 2 2 3 3 . . 3 2 2 
-    2 2 2 2 2 2 2 2 2 2 2 3 3 3 2 2 
-    2 2 2 2 2 2 2 2 2 2 2 2 2 3 3 2 
-    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+scene.setBackgroundColor(1)
+let bimbo = sprites.create(img`
+    . . . . . f f f f f . . . . . . 
+    . . . f f f 2 2 2 f f f . . . . 
+    . . . f 2 2 2 2 2 2 2 f . . . . 
+    . . . f e e e e e e e f . . . . 
+    . . . f e 3 3 3 3 3 e f . . . . 
+    . . . f f 3 7 3 7 3 f f . . . . 
+    . . . . f 3 3 3 3 3 f . . . . . 
+    . . . . f 3 3 3 3 3 f . . . . . 
+    . . f f f f f 3 f f f . . . . . 
+    . . f 3 f 8 8 8 8 8 f f f . . . 
+    . . f 3 3 8 8 8 8 8 3 3 f . . . 
+    . . f f f 8 8 8 8 8 f 3 f . . . 
+    . . . . f 8 8 8 8 8 f f f . . . 
+    . . . . f e e f e e f . . . . . 
+    . . . . f e e f e e f . . . . . 
+    . . . . f e e f e e f . . . . . 
     `, SpriteKind.Player)
